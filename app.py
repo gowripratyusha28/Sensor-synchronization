86286efbbb488abe4afeaf515e34fdbab34caabc
Gowri Pratyusha <pratyusha@Mac.lan>
2025-11-27 15:32:07 -0800

    Moed flask server

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ def create_app():
 
 if __name__ == '__main__':
     app = create_app()
-    socketio.run(host="0.0.0.0", port = 5001, debug=True)
+    socketio.run(app, host="0.0.0.0", port = 5001, debug=True)
